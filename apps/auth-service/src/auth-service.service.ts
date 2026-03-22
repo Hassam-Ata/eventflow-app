6814c84a7833f01cc72dd0fd6476c4085d90d380
Hassam-Ata <hassamata86@gmail.com>
2026-03-22 21:15:32 +0500

    setup kafka

--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthServiceService {
+export class AuthServiceService implements OnModuleInit {
+  constructor(
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  simulateUserRegistration(email: string) {
+    // publish event to kafka
+    this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
+      email,
+      timeStamp: new Date().toISOString(),
+    });
+    return {
+      message: `User registration event for ${email} has been published to Kafka`,
+    };
   }
 }
